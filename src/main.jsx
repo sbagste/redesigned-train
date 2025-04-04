22961ce18d66a0874bbcbbff16ef6e37ad6f264b
sbagste <s-auguste@gmx.com>
2025-04-04 09:20:54 +0200

    🚚 use HashRouter.

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router';
 import './index.css';
 import App from './App.jsx';
 import Creator from './Creator.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='redesigned-train/' element={<App />} />
-      <Route path='redesigned-train/creator' element={<Creator />} />
-    </Routes>
-  </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/creator' element={<Creator />} />
+      </Routes>
+    </HashRouter>
 );
